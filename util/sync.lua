88c5ada6a783943eb4328f08e6ed9e3506ffd44b
最萌小汐（宇宙创生） <sumneko@hotmail.com>
2026-01-29 03:24:31 +0000

    Update

--- util/sync.lua
+++ util/sync.lua
@@ -5,52 +5,13 @@ local M = Class 'Sync'
 ---@private
 M.syncMap = {}
 
----@package
----@type boolean?
-M.singleMode = nil
-
----@package
----@return boolean
-function M.isSingleMode()
-    if M.singleMode ~= nil then
-        return M.singleMode
-    end
-    local count = 0
-    for player in clicli.player_group.get_all_players():pairs() do
-        if player:need_sync() then
-            count = count + 1
-            if count > 1 then
-                clicli.ltimer.wait(60, function ()
-                    M.singleMode = nil
-                end)
-                M.singleMode = false
-                return false
-            end
-        end
-    end
-    M.singleMode = true
-    return true
-end
-
 --Send local messages and use 'onSync' to synchronize receiving data
 --Use this function in your local environment
 ---@param id string # Ids beginning with '$' are reserved for internal use
 ---@param data Serialization.SupportTypes
 function M.send(id, data)
-    if M.isSingleMode() then
-        clicli.ltimer.wait_frame(1, function ()
-            local callback = M.syncMap[id]
-            if not callback then
-                return
-            end
-            clicli.player.with_local(function (local_player)
-                xpcall(callback, log.error, data, local_player)
-            end)
-        end)
-    else
-        local bin = clicli.dump.encode(data)
-        broadcast_lua_msg(id, bin)
-    end
+    local bin = clicli.dump.encode(data)
+    broadcast_lua_msg(id, bin)
 end
 
 --The data is received synchronously, and the callback function is executed after synchronization
