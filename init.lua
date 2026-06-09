ea1aec7cfe51448cc57d7699aa80f4e3c5625b22
最萌小汐（宇宙创生） <sumneko@hotmail.com>
2026-06-09 07:10:53 +0000

    Update

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require 'clicli.debugger'
 ---@class CliCli
 clicli = {}
 
-clicli.version = 260608
+clicli.version = 260609
 
 clicli.proxy   = require 'clicli.tools.proxy'
 clicli.class   = require 'clicli.tools.class'
