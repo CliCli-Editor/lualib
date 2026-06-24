906c5ef446286527574fff023be61ab9cabc9cc6
最萌小汐（宇宙创生） <sumneko@hotmail.com>
2026-06-24 02:59:41 +0000

    Update

--- object/runtime_object/player.lua
+++ object/runtime_object/player.lua
@@ -885,12 +885,12 @@ function M:request_random_number(group_id, key, callback)
         xpcall(callback, log.error, result)
     end, {})
 end
-
----Request the player's open save data
+---Request the player is open save data (module-level function, called using.)
+---@param aid string|integer # The platform of the target player is AID
 ---@param callback fun(archive: any) # Callback function after execution
-function M:request_open_archive(callback)
+function M.request_open_archive(aid, callback)
     ---@diagnostic disable-next-line: undefined-field
-    GameAPI.lua_request_role_open_archive(self.handle, function (context)
+    GameAPI.lua_request_role_open_archive(aid, function (context)
         xpcall(callback, log.error, context['__role_open_archive'])
     end, {})
 end
